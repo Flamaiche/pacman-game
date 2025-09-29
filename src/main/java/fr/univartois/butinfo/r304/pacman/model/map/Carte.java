1bcf729849b49e6191389ccb16f75bb04467945b
Flamaiche <Flamaiche@gmail.com>
2025-09-29 10:49:27 +0200

    generation de la carte

--- src/main/java/fr/univartois/butinfo/r304/pacman/model/map/Carte.java
+++ src/main/java/fr/univartois/butinfo/r304/pacman/model/map/Carte.java
@@ -1,7 +1,32 @@
 package fr.univartois.butinfo.r304.pacman.model.map;
 
+import fr.univartois.butinfo.r304.pacman.model.IAnimated;
+import javafx.beans.binding.IntegerExpression;
+
+import java.util.ArrayList;
+
 /*
-le labyrinth sera genera à partir de plusieurs Carte
+le labyrinth sera genera à partir Carte (le labyrinth est une Carte)
  */
 public class Carte {
+    private int largeur;
+    private int hauteur;
+    private Cell[][] grid;
+
+    public Carte(int largeur, int hauteur) {
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        grid = new Cell[largeur][hauteur];
+        initCell();
+    }
+
+    public void initCell() {
+        for (int x = 0; x < largeur; x++) {
+            for (int y = 0; y < hauteur; y++) {
+                Cell newCell = new Cell(x, y);
+                grid[x][y] = newCell;
+            }
+        }
+    }
+
 }
