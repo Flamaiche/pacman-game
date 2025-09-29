fe2a8ec7c703be47c5ab091db4e603fdbc6de364
Flamaiche <Flamaiche@gmail.com>
2025-09-29 11:13:10 +0200

    creation carte 16.b

--- src/main/java/fr/univartois/butinfo/r304/pacman/model/map/Carte.java
+++ src/main/java/fr/univartois/butinfo/r304/pacman/model/map/Carte.java
@@ -3,6 +3,7 @@ package fr.univartois.butinfo.r304.pacman.model.map;
 import fr.univartois.butinfo.r304.pacman.model.IAnimated;
 import javafx.beans.binding.IntegerExpression;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /*
@@ -11,6 +12,16 @@ Carte est un générateur de Cell pour GameMap
  */
 public class Carte {
     private static final Random rand = new Random();
+
+    public static GameMap createMap(int largeur, int hauteur) {
+        GameMap map = new GameMap(hauteur, largeur);
+        for (int i = 0; i < hauteur; i++) {
+            for (int j = 0; j < largeur; j++) {
+
+            }
+        }
+        return map;
+    }
 
 
 }
