d7318dcbae8533bfc748614e9e5fd95369f0fd1f
malik_babahamou <malik_babahamou@ens.univ-artois.fr>
2025-10-02 12:30:54 +0200

    reset du code aprés une mauvaise correction

--- src/main/java/fr/univartois/butinfo/r304/pacman/model/animated/AbstractAnimated.java
+++ src/main/java/fr/univartois/butinfo/r304/pacman/model/animated/AbstractAnimated.java
@@ -472,6 +472,6 @@ public abstract class AbstractAnimated implements IAnimated {
     }
 
     public void onCollisionWith(IAnimated other) {
-        other.isCollidingWith(this);
+        other.onCollisionWith(this);
     }
 }
