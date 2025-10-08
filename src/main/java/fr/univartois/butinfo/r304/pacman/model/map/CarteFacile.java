266bc12fe4e6ab8985dbbe259253f8834a44588d
Flamaiche <Flamaiche@gmail.com>
2025-10-08 08:55:39 +0200

    nouvelle carte (pas finie)

---
+++ src/main/java/fr/univartois/butinfo/r304/pacman/model/map/CarteFacile.java
@@ -0,0 +1,57 @@
+package fr.univartois.butinfo.r304.pacman.model.map;
+
+import fr.univartois.butinfo.r304.pacman.view.Sprite;
+import fr.univartois.butinfo.r304.pacman.view.SpriteStore;
+
+import java.awt.desktop.SystemSleepEvent;
+
+public class CarteFacile extends Carte {
+
+    public CarteFacile() {}
+
+    @Override
+    public GameMap createMap(int largeur, int hauteur) {
+
+        GameMap map = new Carte().createMap(largeur, hauteur);
+        ajoutMursInterieurs(map, largeur, hauteur);
+        return map;
+    }
+
+    private void ajoutMursInterieurs(GameMap map, int largeur, int hauteur) {
+        SpriteStore spriteStore = new SpriteStore();
+        Wall wall = new Wall(spriteStore.getSprite("wall"));
+        Sprite path = spriteStore.getSprite("path");
+
+        double centreX = (largeur-1) / 2.0;
+        double centreY = (hauteur-1) / 2.0;
+
+        for (int x = 0; x < largeur; x++) {
+            for (int y = 0; y < hauteur; y++) {
+                // coins
+                if ((y == 3 || y == hauteur-4) && ((x > 2 && x <= 5) || (x > largeur-7 && x <= largeur-4))) {
+                    map.setAt(y, x, new Cell(wall));
+                } else if ((x == 3 || x == largeur-4) && ((y > 2 && y <= 5) || (y > hauteur-7 && y <= hauteur-4))) {
+                    map.setAt(y, x, new Cell(wall));
+                // centre
+                } else if (x == (int)centreX && (x < 3 || x > hauteur-3)) {
+                    map.setAt(y, x, new Cell(wall));
+                    if (x < centreX) {
+                        map.setAt(y, x+1, new Cell(wall));
+                    }
+                } else if (y == (int)centreY && (x > 3 && x < largeur-4)) {
+                    map.setAt(y, x, new Cell(wall));
+                    if (y < centreY) {
+                        map.setAt(y+1, x, new Cell(wall));
+                    }
+                } else if (x == (int)centreX && (y > 3 && y < hauteur-4)) {
+                    System.out.println(x +"\n" + centreX + "\n" + largeur);
+                    map.setAt(y, x, new Cell(wall));
+                    if (x < centreX) {
+                        map.setAt(y, x + 1, new Cell(wall));
+                    }
+                }
+            }
+        }
+
+    }
+}
