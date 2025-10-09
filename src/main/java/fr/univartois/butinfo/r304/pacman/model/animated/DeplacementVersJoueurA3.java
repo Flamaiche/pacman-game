9ed4079066d2fcc412d3ed746c6b58027d5c3eee
Flamaiche <Flamaiche@gmail.com>
2025-10-09 08:39:30 +0200

    nouveau deplacement avec anticipation a 3

---
+++ src/main/java/fr/univartois/butinfo/r304/pacman/model/animated/DeplacementVersJoueurA3.java
@@ -0,0 +1,16 @@
+package fr.univartois.butinfo.r304.pacman.model.animated;
+
+public class DeplacementVersJoueurA3 implements IStrategieDeplacement {
+    private final Fantome fantome;
+
+    public DeplacementVersJoueurA3(Fantome fantome) {
+        this.fantome = fantome;
+    }
+
+    @Override
+    public void mouvement() {
+        DeplacementVersJoueur dvj = new DeplacementVersJoueur(fantome);
+        dvj.mouvement(3);
+    }
+
+}
