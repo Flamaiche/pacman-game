4466bc01a0f3d5d5b28729251dbf569f38d17fd5
Flamaiche <Flamaiche@gmail.com>
2025-10-09 08:38:31 +0200

    ajout du deplacement en fonction de la couleur

--- src/main/java/fr/univartois/butinfo/r304/pacman/model/animated/CouleurFantome.java
+++ src/main/java/fr/univartois/butinfo/r304/pacman/model/animated/CouleurFantome.java
@@ -15,5 +15,13 @@ public enum CouleurFantome {
     public String getFolderName() {
         return folderName;
     }
+
+    public IStrategieDeplacement getStrategie(Fantome fantome) {
+        return switch (this) {
+            case PINK -> new DeplacementVersJoueur(fantome);
+            case BLUE -> new DeplacementVersJoueurA3(fantome);
+            default -> new DeplacementAleatoire(fantome);
+        };
+    }
 }
 
